d2c3fa93c7a54579dce61112da788a5c41ff56f1
diyenpatel <202301113@dau.ac.in>
2025-09-19 17:48:07 +0530

    Update main.py
    Implement BaseModel from pydantic

--- main.py
+++ main.py
@@ -1,29 +1,34 @@
-from fastapi import FastAPI, Body
+from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
 class users:
-    id : int
-    username : str
-    password : str
+    id: int
+    username: str
+    password: str
 
-    def _init_(self,id,username,password):
-        self.id=id
-        self.username=username
-        self.password=password
+    def __init__(self, id, username, password):
+        self.id = id
+        self.username = username
+        self.password = password
 
-user_data=[users(1,'hello123','hello'),users(2,'test123','Test12')]
+class UserModel(BaseModel):
+    id: int
+    username: str
+    password: str
+
+user_data = [users(1, "hello123", "hello"), users(2, "test123", "Test12")]
 
 @app.post("/signup")
-async def signup(data=Body()):
-    
-    user_data.append(users(data["id"],data["username"],data["password"]))
-    return {'status' : 'accepted', 'data' : data}
+async def signup(data: UserModel):
+    user_data.append(users(data.id, data.username, data.password))
+    return {"status": "accepted", "data": data}
 
 @app.post("/login")
-async def login(data=Body()):
-    check = users(data["id"],data["username"],data["password"])
+async def login(data: UserModel):
+    check = users(data.id, data.username, data.password)
     for user in user_data:
-        if user.username==check.username and user.password==check.password:
+        if user.username == check.username and user.password == check.password:
             return {"message": "Login Successful"}
     return {"message": "User not found"}
