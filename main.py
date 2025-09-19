681cf65331025fae2a869e5ad97c162fc1dd4f67
PranavMandani16 <202301101@dau.ac.in>
2025-09-19 17:40:52 +0530

    Enhanced Signin Login endpoints
    usage of classes to generalise the data

--- main.py
+++ main.py
@@ -2,17 +2,28 @@ from fastapi import FastAPI, Body
 
 app = FastAPI()
 
-user_data=[]
+class users:
+    id : int
+    username : str
+    password : str
+
+    def _init_(self,id,username,password):
+        self.id=id
+        self.username=username
+        self.password=password
+
+user_data=[users(1,'hello123','hello'),users(2,'test123','Test12')]
 
 @app.post("/signup")
 async def signup(data=Body()):
     
-    user_data.append(data)
+    user_data.append(users(data["id"],data["username"],data["password"]))
     return {'status' : 'accepted', 'data' : data}
 
 @app.post("/login")
 async def login(data=Body()):
+    check = users(data["id"],data["username"],data["password"])
     for user in user_data:
-        if user['username']==data['username']:
-            return {'login Sucessfull'}
-    return {'user not found'}
+        if user.username==check.username and user.password==check.password:
+            return {"message": "Login Successful"}
+    return {"message": "User not found"}
