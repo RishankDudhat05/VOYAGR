71128a96b7fda4606419ca275037d8f9e7b44af6
AadityaPala <202301076@dau.ac.in>
2025-09-19 17:26:11 +0530

    Implement signup and login endpoints

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
-from fastapi import FastAPI,Body
+from fastapi import FastAPI, Body
 
 app = FastAPI()
 
+user_data=[]
+
 @app.post("/signup")
-async def signup(data: Body()):
-  return {"status": "accepted", "data": data}
+async def signup(data=Body()):
+    
+    user_data.append(data)
+    return {'status' : 'accepted', 'data' : data}
+
+@app.post("/login")
+async def login(data=Body()):
+    for user in user_data:
+        if user['username']==data['username']:
+            return {'login Sucessfull'}
+    return {'user not found'}
