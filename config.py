68827e7cba7fd38ee2d72aacec603c1c81d4780c
AadityaPala <202301076@dau.ac.in>
2025-09-21 00:54:40 +0530

    Groq API key from the .env file.
    API keys are not meant to share in public. Created .env file and keep the API key there. Imported using load_dotenv() function.

--- config.py
+++ config.py
@@ -1,3 +1,9 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
 SECRET_KEY = "abcdefghijklmnopqrstuvwxyz"
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
+GROQ_API_KEY = os.getenv("GROQ_API_KEY")
