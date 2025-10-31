03fba7d5f64f15bcd3baffa9d773e9f81e39b2ff
AadityaPala <202301076@dau.ac.in>
2025-10-31 20:39:26 +0530

    Convert User model to Pydantic with ObjectId support
    Refactor User model to use Pydantic and ObjectId for validation.

--- models.py
+++ models.py
@@ -1,10 +1,29 @@
-from sqlalchemy import Column, Integer, String
-from database import Base
+from pydantic import BaseModel, Field
+from typing import Optional
+from bson import ObjectId
 
-class User(Base):
-    __tablename__ = "users"
+class PyObjectId(ObjectId):
+    @classmethod
+    def __get_validators__(cls):
+        yield cls.validate
 
-    id = Column(Integer, primary_key=True, index=True)
-    name = Column(String,unique=True)
-    email_id = Column(String, unique=True, index=True)
-    hashed_password = Column(String)
+    @classmethod
+    def validate(cls, v):
+        if not ObjectId.is_valid(v):
+            raise ValueError("Invalid objectid")
+        return ObjectId(v)
+
+    @classmethod
+    def __get_pydantic_json_schema__(cls, field_schema):
+        field_schema.update(type="string")
+
+class User(BaseModel):
+    id: Optional[PyObjectId] = Field(default=None, alias="_id")
+    name: str
+    email_id: str
+    hashed_password: str
+
+    class Config:
+        populate_by_name = True
+        arbitrary_types_allowed = True
+        json_encoders = {ObjectId: str}
