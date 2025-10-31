31397fce536f8e04125a4bbc71c06fb6978d9c24
AadityaPala <202301076@dau.ac.in>
2025-10-31 20:49:10 +0530

    Update FastAPI app to use async context manager
    Removed sql alchemy imports. added lifespan function to add unique attribute to the email_id, before the app starts, and then closing the connection once the app stops running.

--- main.py
+++ main.py
@@ -1,19 +1,16 @@
-from fastapi import FastAPI, Depends, HTTPException, status
+from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from sqlalchemy.orm import Session
-from database import SessionLocal, engine, Base
-from models import User
-from schemas import UserCreate, UserResponse, Token
-from auth import hash_password, verify_password, create_access_token
-from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
-from jose import JWTError, jwt
-from config import SECRET_KEY, ALGORITHM
-from router import auth_routes,tripai
+from contextlib import asynccontextmanager
+from database import users_collection, client
+from router import auth_routes, tripai
 
-# Create database tables
-Base.metadata.create_all(bind=engine)
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    await users_collection.create_index("email_id", unique=True)
+    yield
+    client.close()
 
-app = FastAPI()
+app = FastAPI(lifespan=lifespan)
 
 app.include_router(tripai.router)
 app.include_router(auth_routes.router)
@@ -25,4 +22,3 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
