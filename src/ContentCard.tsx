80f0e6d8eafe0fe7b76ed24d19685222e532b277
Sharma Agrim Sarju <202301087@dau.ac.in>
2025-11-08 01:21:03 +0530

    Refactor ContentCard styles and classes
    Updated hover styles and class names for better UI.

--- src/ContentCard.tsx
+++ src/ContentCard.tsx
@@ -25,24 +25,25 @@ export default function ContentCard({
       <style>{`
       ${ContentCardStyle}
       .content-card:hover .content-card-heading {
-        background-color: #000;
-        color: #fff;
+        background-color: #f7fbfa;
         border-radius: 0.75rem 0.75rem 0.75rem 0.75rem;
-        border-color: #000;
+        border-color: #f7fbfa;
       }
       `}</style>
       {/* Card container with conditional cursor style */}
       <div
         onClick={onClick}
-        className={`content-card m-2 p-4 bg-gray-300 font-nunito max-w-sm rounded-xl shadow-none border border-1
-             transition hover:shadow-sm hover:scale-[1.01] hover:bg-white${
+        className={`content-card m-2 p-4 bg-mywhite text-myblack font-nunito w-full rounded-lg shadow-none border border-1
+             transition hover:shadow-sm hover:scale-[1.02] hover:bg-myred hover:text-myred${
                onClick ? " cursor-pointer" : ""
              }`}
       >
         <h2 className="content-card-heading text-2xl font-semibold mb-2 px-2 py-2 transition-colors">
           {heading}
         </h2>
-        <p className="mt-2 text-gray-800 text-1xl font-normal">{children}</p>
+        <p className="mt-2 text-myblack text-1xl font-normal transition hover:text-mywhite">
+          {children}
+        </p>
       </div>
     </>
   );
