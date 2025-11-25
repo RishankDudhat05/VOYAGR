745e78b7dd6760ac3152d2e0e6ff505b5c27f12b
princesojitra29 <202301126@dau.ac.in>
2025-11-26 04:44:25 +0530

    Add Vercel app URL to CORS allowed origins

--- main.py
+++ main.py
@@ -17,8 +17,12 @@ app.include_router(auth_routes.router)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=[
+        "http://localhost:5173",
+        "https://voyagrbyg13.vercel.app"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
