ced5d8299886a85d1b9b8c78b3134de402e5d995
diyenpatel <202301113@dau.ac.in>
2025-11-29 07:00:37 +0530

    Add recommendations and manual routers to FastAPI

--- main.py
+++ main.py
@@ -2,11 +2,10 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from contextlib import asynccontextmanager
 from database import users_collection, client
-from router import auth_routes, tripai
+from router import auth_routes, tripai, recommendations ,manual
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    await users_collection.create_index("email_id", unique=True)
     yield
     client.close()
 
@@ -14,16 +13,15 @@ app = FastAPI(lifespan=lifespan)
 
 app.include_router(tripai.router)
 app.include_router(auth_routes.router)
+app.include_router(recommendations.router)
+app.include_router(manual.router)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:5173",
-        "https://voyagrg13.vercel.app",
-        "https://voyagrg13-two.vercel.app"
-    ],
+    allow_origins=["http://localhost:5173",
+                   "https://voyagrg13-two.vercel.app"
+                  ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
